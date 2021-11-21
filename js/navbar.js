b5c17c40008b1d91eb1c8b35732593c7dbb7bff5
Lucinda <lucindaly03@gmail.com>
2021-11-21 15:40:14 +1100

    nav active when scrolled

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,9 @@
 const navMenu = document.querySelectorAll('.nav_menu');
+const sections =document.querySelectorAll('section');
 
+/** Click events for nav**/
 navMenu.forEach((nav) => {
-    nav.addEventListener('click', () => {
+    nav.addEventListener('click', (e) => {
         removeActiveNav();
         nav.classList.add('active');
     })
@@ -13,3 +15,25 @@ const removeActiveNav = () => {
         nav.classList.remove('active');
     })
 }
+
+/** Scroll event for nav -> set the nav class to active when scrolled to that section**/
+const setToActive = () =>{
+    for (const section of sections){
+
+        const rect = section.getBoundingClientRect();
+
+        if(rect.y >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) || rect.y <= 90){
+            const sectionId = section.getAttribute('id');
+            navMenu.forEach((nav) => {
+                const navHref = nav.getAttribute('href').slice(1);
+                if (sectionId === navHref) {
+                    removeActiveNav();
+                    nav.classList.add('active');
+                }
+            })
+       }
+    }
+}
+
+//DOM listens to the scroll, and runs the setToActive function
+document.addEventListener('scroll', () =>{setToActive();});
